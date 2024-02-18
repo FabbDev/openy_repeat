cf830118feb5c9568e344ee163f0642e95962ffa
fox mulder <14402-foxmulder@users.noreply.drupalcode.org>
2024-02-18 18:35:54 +0100

    Issue #3420684 by fox mulder: Non-existent field type names in annotation of GeolocationGeometryWidgetLeaflet

--- modules/geolocation_geometry/src/Plugin/Field/FieldType/GeolocationGeometryBase.php
+++ modules/geolocation_geometry/src/Plugin/Field/FieldType/GeolocationGeometryBase.php
@@ -157,8 +157,14 @@ abstract class GeolocationGeometryBase extends FieldItemBase {
   protected static function getRandomCoordinates(array $reference_point = NULL, int $range = 5) {
     if ($reference_point) {
       return [
-        'latitude' => rand(max(-89, ($reference_point['latitude'] - $range)), min(90, ($reference_point['latitude'] + $range))) - (float) (rand(0, 999999) / 1000000),
-        'longitude' => rand(max(-179, ($reference_point['longitude'] - $range)), min(180, ($reference_point['longitude'] + $range))) - (float) (rand(0, 999999) / 1000000),
+        'latitude' => rand(
+          max(-89, ($reference_point['latitude'] - $range)),
+          min(90, ($reference_point['latitude'] + $range))
+        ) - (float) (rand(0, 999999) / 1000000),
+        'longitude' => rand(
+          max(-179, ($reference_point['longitude'] - $range)),
+          min(180, ($reference_point['longitude'] + $range))
+        ) - (float) (rand(0, 999999) / 1000000),
       ];
     }
     return [
